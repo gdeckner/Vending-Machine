cb43d5aa12c4cfd69185925fe3253da57e9bd59b
Georg Deckner <georgdeckner@gmail.com>
2019-06-07 09:22:01 -0400

    small update + test

---
+++ Georg Copy Captsone/Capstone/Classes/VendingMachineItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Classes
+{
+    public class VendingMachineItem
+    {
+        public string Slot { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public int Quantity = 5;
+        
+       
+    }
+        
+        
+        
+}
---
+++ Georg Copy Captsone/CapstoneTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Capstone.Classes;
+
+namespace CapstoneTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+      
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+        
+    }
+}
